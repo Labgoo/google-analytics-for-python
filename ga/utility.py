46313200ff241c259bfa899327fbe702e65f74d2
Minh Tu Le <minhtu@labgoo.com>
2013-07-29 20:07:19 +0300

    changed logger's string format to match gae's one

--- ga/utility.py
+++ ga/utility.py
@@ -7,7 +7,7 @@ GA_LOGGING_LEVEL = logging.DEBUG
 
 
 def init_ga_logger():
-    formatter = logging.Formatter("%(levelname)-9s %(asctime)s \t %(filename)s:%(lineno)d] %(message)s")
+    formatter = logging.Formatter("%(levelname)-8s %(asctime)s \t %(filename)s:%(lineno)d [Google Analytics] %(message)s")
     handler = logging.StreamHandler()
     handler.setFormatter(formatter)
 
